db20a440015e90385bed3607963c64c4d5d06707
Jeffrey McCrea <duldren@gmail.com>
2018-06-10 13:24:12 -0700

    Add createMessage emit and newMessage handler

--- public/js/index.js
+++ public/js/index.js
@@ -18,4 +18,26 @@ var createEmail = function (newEmail, text) {
     email: newEmail,
     text: text
   });
-}
+};
+
+
+var createMessage = function(from, text) {
+  var data = {from: from, text: text}
+  socket.emit('createMessage', data);
+};
+
+socket.on('newMessage', function (data) {
+  console.log(JSON.stringify(data));
+});
+
+
+// challenge
+/* 
+SERVER -> CLIENT
+emit event - newMessage {from, text, createdAt}
+client listens for newMessage
+
+SERVER <- CLIENT
+emit event - createMessage {from, text}
+server listens for 'createMessage'
+*/
