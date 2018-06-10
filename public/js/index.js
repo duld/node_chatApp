5b99a433e5321d81d97ce7a67b719d0e5f28fc7a
Jeffrey McCrea <duldren@gmail.com>
2018-06-10 12:41:31 -0700

    Add createEmail event index.js

--- public/js/index.js
+++ public/js/index.js
@@ -6,4 +6,16 @@ socket.on('connect', function () {
 
 socket.on('disconnect', function () {
   console.log('connection closed.');
-})
+});
+
+socket.on('newEmail', function (data) {
+  console.log('New Email!');
+  console.log(data);
+});
+
+var createEmail = function (newEmail, text) {
+  socket.emit('createEmail', {
+    email: newEmail,
+    text: text
+  });
+}
