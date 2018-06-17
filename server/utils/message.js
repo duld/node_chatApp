05a51d92dd16cfc3d1406d6518d0ff5ef7daba60
Jeffrey McCrea <duldren@gmail.com>
2018-06-17 11:58:31 -0700

    Add generateLocationMessage util

--- server/utils/message.js
+++ server/utils/message.js
@@ -2,6 +2,15 @@ const generateMessage = (from, text) => {
   return {from, text, createdAt: Date.now()}
 }
 
+const generateLocationMessage = (from, lat, lon) => {
+  return {
+    from,
+    url: `https://google.com/maps?q=${lat},${lon}`,
+    createdAt: Date.now()
+  };
+}
+
 module.exports = {
-  generateMessage
+  generateMessage,
+  generateLocationMessage
 }
