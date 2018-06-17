b619d9cff3194f9c59bf970e6d3d94846246ecfc
Jeffrey McCrea <duldren@gmail.com>
2018-06-17 09:09:51 -0700

    Add comments to index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,22 @@
 var socket = io();
 
+// connect - handler
 socket.on('connect', function () {
   console.log('connected to server');
 });
 
+// disconnect - handler
 socket.on('disconnect', function () {
   console.log('connection closed.');
 });
 
+// newEmail - handler
 socket.on('newEmail', function (data) {
   console.log('New Email!');
   console.log(data);
 });
 
+// createEmail - event emit
 var createEmail = function (newEmail, text) {
   socket.emit('createEmail', {
     email: newEmail,
@@ -20,12 +24,13 @@ var createEmail = function (newEmail, text) {
   });
 };
 
-
+// createMessage - event emit
 var createMessage = function(from, text) {
   var data = {from: from, text: text}
   socket.emit('createMessage', data);
 };
 
+// newMessage - handler
 socket.on('newMessage', function (data) {
   console.log(JSON.stringify(data));
 });
